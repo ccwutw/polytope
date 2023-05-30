08a4996003dfd1c44c73db185385a02b533c82f9
Adam Warde <adam.warde@ecmwf.int>
2023-05-30 17:00:01 +0200

    Added tests for point and radius

--- tests/test_EDR_interface.py
+++ tests/test_EDR_interface.py
@@ -24,13 +24,13 @@ from polytope.shapes import (
 class TestEDRInterface:
     def setup_method(self, method):
         # Create a dataarray with 3 labelled axes using different index types
-        dims = np.random.randn(3, 6, 129)
+        dims = np.random.randn(3, 200, 200)
         array = xr.Dataset(
-            data_vars=dict(param=(["date", "step", "level"], dims)),
+            data_vars=dict(param=(["date", "lat", "long"], dims)),
             coords={
                 "date": pd.date_range("2000-01-01", "2000-01-03", 3),
-                "step": [0, 3, 6, 9, 12, 15],
-                "level": range(1, 130),
+                "lat": range(0,200),
+                "long": range(0, 200),
             },
         )
         self.xarraydatacube = XArrayDatacube(array)
@@ -40,12 +40,23 @@ class TestEDRInterface:
     # Testing different shapes
 
     def test_2D_box(self):
-        request = Request(Box(["step", "level"], [3, 10], [6, 11]), Select("date", ["2000-01-01"]))
+        request = Request(Box(["lat", "long"], [3, 10], [6, 11]), Select("date", ["2000-01-01"]))
         result = self.API.retrieve(request)
-        assert len(result.leaves) == 4
+        print(result)
+        assert len(result.leaves) == 8
 
     def test_EDR_point(self):
-        p1 = wkt.loads('POINT(0 51.48)')
-        #request = Request(Select(["step", "level"], [p1.x, p1.y]))
+        p1 = wkt.loads('POINT(0 51)')
+        request = Request(Select("date", ["2000-01-03"]), Select("lat", [p1.x]), Select("long", [p1.y]))
+        result = self.API.retrieve(request)
         assert p1.x == 0
-        assert p1.y == 51.48
+        assert p1.y == 51
+
+    def test_EDR_radius(self):
+        p1 = wkt.loads('POINT(0 51)')
+        radius = 3
+        request = Request(Disk(["lat", "long"], [p1.x, p1.y], [radius, radius]), Select("date", ["2000-01-01"]))
+        result = self.API.retrieve(request)
+        assert len(result.leaves) == 18
+
+    #def test_EDR_polygon(self):
